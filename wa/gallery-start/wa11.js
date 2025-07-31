f66986255ee7ed47d77dead6d22a0eab88604279
eterjot75 <etrejot75@gmail.com>
2025-07-30 23:57:22 -0600

    wa11

---
+++ wa/gallery-start/wa11.js
@@ -0,0 +1,37 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+
+const alternativeText = {
+    'pic1.jpg': 'Closeup of a human eye',
+    'pic2.jpg': 'Rock that looks like a wave',
+    'pic3.jpg': 'Purple and white pansies',
+    'pic4.jpg': 'Section of wall from a pharaoh\'s tomb',
+    'pic5.jpg': 'Large moth on a leaf'
+};
+
+for (const fileName of imageFileName) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', xxx);
+    newImage.setAttribute('alt', xxx);
+    thumbBar.appendChild(newImage);
+}
+    
+
+btn.addEventListener('click', () => {
+  const currentMode = btn.getAttribute('class');
+
+  if (currentMode === 'dark') {
+    btn.setAttribute('class', 'light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+  } else {
+    btn.setAttribute('class', 'dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgb(0 0 0 / 0%)';
+  }
+});
