2113f934b2b5dd0e2a5964ff9bdb74f771d43acb
eterjot75 <etrejot75@gmail.com>
2025-07-31 01:15:50 -0600

    wa11 updated

--- wa/gallery-start/wa11.js
+++ wa/gallery-start/wa11.js
@@ -1,37 +1,45 @@
-const displayedImage = document.querySelector('.displayed-img');
-const thumbBar = document.querySelector('.thumb-bar');
+const displayedImage = document.querySelector(".displayed-img");
+const thumbBar = document.querySelector(".thumb-bar");
 
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
+const btn = document.querySelector("button");
+const overlay = document.querySelector(".overlay");
 
-const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+const images = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.png"];
 
 const alternativeText = {
-    'pic1.jpg': 'Closeup of a human eye',
-    'pic2.jpg': 'Rock that looks like a wave',
-    'pic3.jpg': 'Purple and white pansies',
-    'pic4.jpg': 'Section of wall from a pharaoh\'s tomb',
-    'pic5.jpg': 'Large moth on a leaf'
+    "pic1.jpg": "Abstract scenery",
+    "pic2.jpg": "Trailers driving down street",
+    "pic3.jpg": "Green Northern Lights",
+    "pic4.jpg": "Beach sunset with plam trees",
+    "pic5.jpg": "Abstract castle on hill scene"
 };
 
-for (const fileName of imageFileName) {
-    const newImage = document.createElement('img');
-    newImage.setAttribute('src', xxx);
-    newImage.setAttribute('alt', xxx);
+for (const fileName of images) {
+
+    const newImage = document.createElement("img");
+    newImage.setAttribute("src", `images/${fileName}` );
+    newImage.setAttribute("alt", alternativeText[fileName]);
     thumbBar.appendChild(newImage);
+
+    newImage.addEventListener("click", () => {
+        displayedImage.setAttribute("src", `images/${fileName}`);
+        displayedImage.setAttribute("alt", alternativeText[fileName]);
+    });
 }
     
 
-btn.addEventListener('click', () => {
-  const currentMode = btn.getAttribute('class');
+btn.addEventListener("click", () => {
 
-  if (currentMode === 'dark') {
-    btn.setAttribute('class', 'light');
-    btn.textContent = 'Lighten';
-    overlay.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+  const currentMode = btn.getAttribute("class");
+
+  if (currentMode === "dark") {
+    btn.setAttribute("class", "light");
+    btn.textContent = "Lighten";
+    overlay.style.backgroundColor = "rgb(0 0 0 / 50%)";
   } else {
-    btn.setAttribute('class', 'dark');
-    btn.textContent = 'Darken';
-    overlay.style.backgroundColor = 'rgb(0 0 0 / 0%)';
+    btn.setAttribute("class", "dark");
+    btn.textContent = "Darken";
+    overlay.style.backgroundColor = "rgb(0 0 0 / 0%)";
+
   }
 });
